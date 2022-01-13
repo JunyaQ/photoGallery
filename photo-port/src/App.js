5d728cdb85353550abfb2b170766aff703b9b2a5
Junya Qiao <junyaqiao@gmail.com>
2022-01-12 19:55:58 -0500

    navigate part

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import About from './components/About'
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-  <About></About>
-      
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }

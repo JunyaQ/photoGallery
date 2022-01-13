7dcd9eb1f6130e4449afe5f0480a70f5181ef8c2
Junya Qiao <junyaqiao@gmail.com>
2022-01-13 01:01:33 -0500

    submit contact form and only show on contact info page

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -17,17 +17,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-      <ContactForm></ContactForm>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
